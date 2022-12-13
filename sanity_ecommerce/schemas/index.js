308e4b8d325a0304b0c414982c9f6d8c31177ed6
farhanahzan <41515998+farhanahzan@users.noreply.github.com>
2022-12-13 10:06:22 +0530

    test

--- sanity_ecommerce/schemas/index.js
+++ sanity_ecommerce/schemas/index.js
@@ -1,1 +1,5 @@
-export const schemaTypes = []
+import product from './product'
+import banner from './banner'
+import category from './category'
+import tag from './tag'
+export const schemaTypes = [product, category, tag, banner]
--- sanity_ecommerce/schemas/product.js
+++ sanity_ecommerce/schemas/product.js
@@ -1,3 +1,85 @@
 export default {
   name: 'product',
+  title: 'Product',
+  type: 'document',
+  fields: [
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'array',
+      of: [{type: 'image'}],
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 90,
+      },
+    },
+    {
+      name: 'originalprice',
+      title: 'Original Price',
+      type: 'number',
+    },
+    {
+      name: 'saleprice',
+      title: 'Sale Price',
+      type: 'number',
+    },
+    {
+      name: 'shortdesc',
+      title: 'Short Description',
+      type: 'string',
+    },
+    {
+      name: 'fulldesc',
+      title: 'Full Description',
+      type: 'string',
+    },
+    {
+      name: 'sku',
+      title: 'SKU',
+      type: 'string',
+    },
+    {
+      name: 'category',
+      title: 'Categories',
+      type: 'array',
+      of: [
+        {
+          type: 'reference',
+          to: [
+            {
+              type: 'category',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: 'tag',
+      title: 'Tag',
+      type: 'array',
+      of: [
+        {
+          type: 'reference',
+          to: [
+            {
+              type: 'tag',
+            },
+          ],
+        },
+      ],
+    },
+  ],
 }
